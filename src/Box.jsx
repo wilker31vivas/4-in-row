ce4ec76e9caabb72ccbcb3706eadcb08dd898555
Wilker <wilker.vivas.31@gmail.com>
2024-08-03 00:32:19 -0400

    version1

--- src/Box.jsx
+++ src/Box.jsx
@@ -6,7 +6,6 @@ export function Box({ id, col }) {
     const { turn, setTurn, arrayCol, setArrayCol } = useContext(GameContext)
     const [circulo, setCirculo] = useState(false)
     const [isClicked, setIsClicked] = useState(false);
-    const [color, setColor] = useState("blue")
 
     const handleClick = (e) => {
         const updatedArray = [...arrayCol];
@@ -22,11 +21,16 @@ export function Box({ id, col }) {
 
         setTurn(prev => !prev)
         setCirculo(true)
-        setColor(turn ? 'blue' : 'red');
         setIsClicked(true);
     };
 
     const DetectarColumna = () => {
+        let color;
+        if(turn){
+            color = "blue";
+        } else {
+            color = "red"
+        }
         if (arrayCol[col][id] == true) {
            return <Circle color={color}></Circle>
          } 
--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -19,9 +19,7 @@ export function GameContextProvider({ children }) {
         setTurn(true)
     }
 
-    useEffect(()=>{
-        console.log("longi", arrayCol[0].length)
-    },[arrayCol])
+    
 
 
     return (
