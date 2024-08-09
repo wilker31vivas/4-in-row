c8523599ddf85b2cb4f74c62f50a33c7287a5f5e
Wilker <wilker.vivas.31@gmail.com>
2024-08-09 00:09:49 -0400

    se optimizo las funciones

--- src/logic/constant.js
+++ src/logic/constant.js
@@ -15,6 +15,14 @@
 // 5: {filled: true, color: 'blue'}
 export const wins = []
 
+function detectarWin(arrayWin){
+    const isTrue = arrayWin.every(item => item.filled === true);
+    const isColor = arrayWin.every((item, _, arr) => item.color === arr[0].color);
+    if(isTrue && isColor){
+        return true
+    } 
+}
+
 //21 posibilidades
 export function resolveCol(array){
     for (let index = 0; index < array.length; index++) {
@@ -30,14 +38,6 @@ export function resolveCol(array){
         const win2 = [item2, item3, item4, item5]
         const win3 = [item3, item4, item5, item6]
 
-        function detectarWin(arrayWin){
-            const isTrue = arrayWin.every(item => item.filled === true);
-            const isColor = arrayWin.every((item, _, arr) => item.color === arr[0].color);
-            if(isTrue && isColor){
-                return true
-            } 
-        }
-
         if (detectarWin(win1) || detectarWin(win2) || detectarWin(win3)) {
             wins[0] = true;
         }
@@ -48,6 +48,30 @@ export function resolveCol(array){
 
 //24 posibilidades
 export function resolveRow(array, rowNum){
+    for (let index = 0; index < array.length; index++) {
+
+        const item1 = array[0][rowNum]
+        const item2 = array[1][rowNum]
+        const item3 = array[2][rowNum]
+        const item4 = array[3][rowNum]
+        const item5 = array[4][rowNum]
+        const item6 = array[5][rowNum]
+        const item7 = array[6][rowNum]
+
+        const win1 = [item1, item2, item3, item4]
+        const win2 = [item4, item5, item6, item7]
+        const win3 = [item2, item3, item4, item5]
+        const win4 = [item3, item4, item5, item6]
+
+        if (detectarWin(win1) || detectarWin(win2) || detectarWin(win3) || detectarWin(win4)) {
+            wins[1] = true;
+        }
+        
+    }
+    return false
+}
+
+export function resolveDiagonal(array, rowNum){
     for (let index = 0; index < array.length; index++) {
 
         const item1 = array[0][rowNum]
